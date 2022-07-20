cc81d8ec87e674ccfcf91229ac8ea40c174b26e9
Kyle Mendell <kmlabsdevl@outlook.com>
2022-07-20 12:57:31 -0500

    Added HuskyParser files

---
+++ utils/huskyparse.go
@@ -0,0 +1,24 @@
+package utils
+
+type HuskyParseObject struct {
+	Type          string
+	InputFilePath string
+	Index         int
+	Line          string
+}
+
+func HuskyParseStart(object HuskyParseObject) {
+
+}
+
+func huskyParseStringVaribles() {
+
+}
+
+func huksyParseIntVaribles() {
+
+}
+
+func huskyParseProject() {
+
+}

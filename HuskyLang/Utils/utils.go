2327113dc5e75789501a295bf4aa898f6a822abb
Kyle Mendell <kmendell@fastenal.com>
2024-07-11 20:25:26 -0500

    Updated failsafe to auto find and run .husky files

--- HuskyLang/Utils/utils.go
+++ HuskyLang/Utils/utils.go
@@ -3,7 +3,10 @@ package Utils
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"ofkm.us/husky/HuskyLang/Parser"
 	"ofkm.us/husky/HuskyLang/Types"
@@ -12,7 +15,7 @@ import (
 func OpenFileFromArg() {
 
 	if len(os.Args) != 2 {
-		fmt.Println("No Husky File Given")
+		FindHuskyFiles()
 	} else {
 		filePath := os.Args[1]
 		readFile, err := os.Open(filePath)
@@ -34,4 +37,38 @@ func OpenFileFromArg() {
 			Parser.HuskyParseFile(Types.HuskyParseObject{InputFilePath: filePath, Index: index, Line: line, Array: fileLines})
 		}
 	}
+}
+
+func walk(s string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if !d.IsDir() {
+		if strings.Contains(s, ".husky") {
+			filePath := s
+			readFile, err := os.Open(filePath)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			fileScanner := bufio.NewScanner(readFile)
+			fileScanner.Split(bufio.ScanLines)
+			var fileLines []string
+
+			for fileScanner.Scan() {
+				fileLines = append(fileLines, fileScanner.Text())
+			}
+
+			readFile.Close()
+
+			for index, line := range fileLines {
+				Parser.HuskyParseFile(Types.HuskyParseObject{InputFilePath: filePath, Index: index, Line: line, Array: fileLines})
+			}
+		}
+	}
+	return nil
+}
+
+func FindHuskyFiles() {
+	filepath.WalkDir("./", walk)
 }
